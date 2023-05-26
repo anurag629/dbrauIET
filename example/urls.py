466b449ac86ea732a70697553d4056375a041fa0
anurag629 <akvermaav629@gmail.com>
2023-05-26 13:24:04 +0530

    completed matrix

--- example/urls.py
+++ example/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 
 from example.views import index
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('', index),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
